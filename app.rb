83dbbca4a42eb453b2e8e36940d6e03a493d62eb
Andrew <andy_htun@hotmail.com>
2015-12-07 17:31:03 +0000

    forms added

--- app.rb
+++ app.rb
@@ -8,7 +8,14 @@ get '/secret' do
   "This is a secret message for you"
 end
 
-get '/cat' do
-  @random_name = ["Amigo", "Oscar", "Viking"].sample
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  @age = params[:age]
   erb(:index)
 end
